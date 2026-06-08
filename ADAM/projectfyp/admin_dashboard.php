f54183bfc393ce728aff495bc142266d6fd3e553
329adam <achia7483@gmail.com>
2026-06-08 18:58:56 +0800

    Delete ADAM/projectfyp directory

--- ADAM/projectfyp/admin_dashboard.php
+++
@@ -1,6 +0,0 @@
-<?php
-// admin_dashboard.php
-session_start();
-header("Location: admin_home.php");
-exit();
-?>
--- ADAM/projectfyp/parent_dashboard.php
+++
@@ -1,6 +0,0 @@
-<?php
-// parent_dashboard.php
-session_start();
-header("Location: parent_home.php");
-exit();
-?>
